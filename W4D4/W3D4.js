33580f6f83ca432f6441923939bf4205e907c6c6
Yosef-Mag <yosef.maghaireh@gmail.com>
2020-04-28 09:40:45 +0300

    Solved toy problem W4D4

--- W4D4/W3D4.js
+++ W4D4/W3D4.js
@@ -7,7 +7,15 @@ countNumOfStrings(["foo", 2, 10, "bar", 5, false, {}, "baz"]); //3
 countNumOfStrings(["There is only one string in this example"]); //1
 Hint: Recall typeof? If not: check it out on MDN. :-)
 */
-
+function countNumOfStrings(array){
+	var result =0;
+	for(var i= 0;i<array.length;i++){
+		if (typeof(array[i])==="string") {
+			result= result+1
+		}
+	}
+	return result
+}
 
 /*
 2- Write a function called  listLengthOfAllWords that takes an array of words (strings), 
@@ -18,14 +26,24 @@ Calling your function should result in:
 listLengthOfAllWords(['hello', 'world', 'I', 'can', 'code']); //[5, 5, 1, 3, 4];
 
 */
+function  listLengthOfAllWords(array){
+	var result=[];
+	for (var i = 0; i < array.length; i++) {
+		result.push(array[i].length)
+	}
+	return result
+}
 
-/*
-Write a function called flipPairs that takes a string a parameter 
-and returns the given string after Flip every pair of characters.
-Example:
+// Write a function called flipPairs that takes a string a parameter 
+// and returns the given string after Flip every pair of characters.
+// Example:
 var input = 'check out how interesting this problem is, it\'s insanely interesting!';
 var output = flipPairs(input);
-console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
+// console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
+
+function flipPairs(str){
+  
+}
 
 
 
